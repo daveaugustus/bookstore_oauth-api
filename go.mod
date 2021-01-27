283e8371dc3728733e1e61fdc977a66adc29eef6
Pappu Kumar <36985548+davetweetlive@users.noreply.github.com>
2021-01-27 11:05:14 +0530

    Project Structure cont... (#1)
    * Project Structure cont...
    
    * structure ready

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/davetweetlive/bookstore_oauth-api
 
 go 1.13
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/stretchr/testify v1.7.0
+)
---
+++ src/domain/access_token/service.go
@@ -0,0 +1,37 @@
+package access_token
+
+import (
+	"strings"
+
+	"github.com/davetweetlive/bookstore_oauth-api/src/utils/errors"
+)
+
+type Repository interface {
+	GetById(string) (*AccessToken, *errors.RestErr)
+}
+
+type Service interface {
+	GetById(string) (*AccessToken, *errors.RestErr)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repo Repository) Service {
+	return &service{
+		repository: repo,
+	}
+}
+
+func (s *service) GetById(accessTokenId string) (*AccessToken, *errors.RestErr) {
+	accessTokenId = strings.TrimSpace(accessTokenId)
+	if len(accessTokenId) == 0 {
+		return nil, errors.NewBadRequestError("invalid access token id")
+	}
+	accessToken, err := s.repository.GetById(accessTokenId)
+	if err != nil {
+		return nil, err
+	}
+	return accessToken, nil
+}
